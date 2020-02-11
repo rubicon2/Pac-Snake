e8d781e172b48fd5910e1dd122c23d318c3d1b5e
Liam Argent <liargent@hotmail.com>
2020-02-11 21:47:42 +0000

    Broke and then fixed again! Starting with animations and end screen.

--- index.js
+++ index.js
@@ -1,14 +1,23 @@
 // Array of players, contains arrays of each player's snake chunk positions. 
 const GAME = document.getElementById("game"); 
 const SCORE = document.getElementById("score");
-const GAME_WIDTH = 400; 
-const GAME_HEIGHT = 400;
+const GAME_WIDTH = GAME.offsetWidth; 
+const GAME_HEIGHT = GAME.offsetHeight;
 const OBJECT_SIZE = 25;
 const MAX_X_POS = GAME_WIDTH - OBJECT_SIZE;
 const MAX_Y_POS = GAME_HEIGHT - OBJECT_SIZE;
 
 var moveInterval = null;
 var foodTimeout = null; 
+
+// Game states.
+const TITLE_SCREEN = 0;
+const PLAYERS_SCREEN = 1;
+const SETTINGS_SCREEN = 2;
+const GAME_SCREEN = 3;
+const GAME_OVER_SCREEN = 4;
+
+var state = TITLE_SCREEN;
 
 // In place of enumeration - not sure if js properly does those. 
 const UP = 1;
@@ -46,33 +55,117 @@ const PLAYER_4_LEFT = 100;
 const startPositionsX = [50, 325,  50, 325];
 const startPositionsY = [50, 325, 325,  50]; 
 const playerColors = ["yellow", "rgb(0, 220, 213)", "rgb(213, 50, 0)", "rgb(30, 190, 0)"];
+const playerClasses = ["player1", "player2", "player3", "player4"];
 const initLength = 3;
-const snakes = []; 
-const supermarket = [];
+var snakes = []; 
+var supermarket = [];
 
-function start(players) {
+function clearScreen() {
 
-    // Clear the play space. 
+    clearInterval(moveInterval);
+    clearTimeout(foodTimeout);
+    moveInterval = null;
+    foodTimeout = null; 
+    snakes = [];
+    supermarket = [];
 
-    let startTexts = document.getElementsByClassName("startText"); 
-    // Omitted i++ on purpose as it never iterated through the whole list. The length keeps changing as you remove elements, 
-    // meaning you want to keep removing the element at index 0. There has to be a better way to do this... 
-    for (let i = 0; i < startTexts.length;) {
-        startTexts[i].remove();
+    let children = document.querySelectorAll("#game *"); 
+    for (let i = 0; i < children.length; i++) {
+        children[i].remove();
     }
+}
 
-    let otherStuff = document.getElementsByTagName("p");
-    for (let i = 0; i < otherStuff.length;) {
-        otherStuff[i].remove();
-    }
+function showTitle() {
+    clearScreen(); 
+    let title = document.createElement("a");
+    title.className = "interactiveText";
+    title.innerText = "PAC-SNAKE";  
+    title.href = "javascript:showPlayersScreen()";
+    GAME.appendChild(title);
+}
 
-    // Create the number of snakes we need and their 3 initial chunks. 
+addEventListener("load", function() {
+    showTitle();
+})
 
+function showPlayersScreen() {
+
+    clearScreen(); 
+
+    let pl1 = document.createElement("a"); 
+    let pl2 = document.createElement("a");
+    let pl3 = document.createElement("a");
+    let pl4 = document.createElement("a");
+
+    pl1.id = "onePlayer";
+    pl2.id = "twoPlayers";
+    pl3.id = "threePlayers";
+    pl4.id = "fourPlayers";
+
+    pl1.className = "interactiveText";
+    pl2.className = "interactiveText";
+    pl3.className = "interactiveText";
+    pl4.className = "interactiveText";
+
+    pl1.innerText = "ONE PLAYER \n";
+    pl2.innerText = "TWO PLAYERS \n"; 
+    pl3.innerText = "THREE PLAYERS \n"; 
+    pl4.innerText = "FOUR PLAYERS"; 
+
+    pl1.href = "javascript:start(1)";
+    pl2.href = "javascript:start(2)";
+    pl3.href = "javascript:start(3)";
+    pl4.href = "javascript:start(4)";
+
+    GAME.appendChild(pl1);
+    GAME.appendChild(pl2);
+    GAME.appendChild(pl3);
+    GAME.appendChild(pl4);
+}
+
+function showSettingsScreen() {
+    clearScreen(); 
+}
+
+function showEndScreen() {
+
+    clearScreen();
+
+    let pl1 = document.createElement("a"); 
+    let pl2 = document.createElement("a");
+    let pl3 = document.createElement("a");
+    let pl4 = document.createElement("a");
+
+    pl1.className = "interactiveText";
+    pl2.className = "interactiveText";
+    pl3.className = "interactiveText";
+    pl4.className = "interactiveText";
+
+    pl1.innerText = `THE LAST SNAKE STANDING WAS `;
+    pl2.innerText = "TWO PLAYERS \n"; 
+    pl3.innerText = "THREE PLAYERS \n"; 
+    pl4.innerText = "FOUR PLAYERS"; 
+
+    pl1.href = "javascript:start(1)";
+    pl2.href = "javascript:start(2)";
+    pl3.href = "javascript:start(3)";
+    pl4.href = "javascript:start(4)";
+
+    GAME.appendChild(pl1);
+    GAME.appendChild(pl2);
+    GAME.appendChild(pl3);
+    GAME.appendChild(pl4);
+}
+
+function createSnakes(players) {
     for (let i = 0; i < players; i++) {
         let newSnake = new Object();
         newSnake.x = [];
         newSnake.y = [];
         newSnake.active = true;
+        newSnake.score = 0;
+        newSnake.nextMoveDir = null;
+        newSnake.lastMoveDir = null; 
 
         newSnake.targetLength = initLength; 
         newSnake.x[0] = startPositionsX[i];
@@ -82,42 +175,53 @@ function start(players) {
         // Set initial chunk positions and movement direction for each snake, depending on player number. 
         switch(i) {
             case 0:
-                newSnake.moveDir = UP;
+                newSnake.nextMoveDir = UP;
                 newSnake.x[1] = newSnake.x[0];
-                newSnake.y[1] = newSnake.y[0] + 25;
+                newSnake.y[1] = newSnake.y[0] + OBJECT_SIZE;
                 newSnake.x[2] = newSnake.x[0];
-                newSnake.y[2] = newSnake.y[0] + 50;
+                newSnake.y[2] = newSnake.y[0] + (OBJECT_SIZE * 2);
                 break;
             case 1: 
-                newSnake.moveDir = DOWN;
+                newSnake.nextMoveDir = DOWN;
                 newSnake.x[1] = newSnake.x[0];
-                newSnake.y[1] = newSnake.y[0] - 25;
+                newSnake.y[1] = newSnake.y[0] - OBJECT_SIZE;
                 newSnake.x[2] = newSnake.x[0];
-                newSnake.y[2] = newSnake.y[0] - 50;
+                newSnake.y[2] = newSnake.y[0] - (OBJECT_SIZE * 2);
                 break;
             case 2:
-                newSnake.moveDir = LEFT;
-                newSnake.x[1] = newSnake.x[0] + 25;
+                newSnake.nextMoveDir = LEFT;
+                newSnake.x[1] = newSnake.x[0] + OBJECT_SIZE;
                 newSnake.y[1] = newSnake.y[0];
-                newSnake.x[2] = newSnake.x[0] + 50;
+                newSnake.x[2] = newSnake.x[0] + (OBJECT_SIZE * 2);
                 newSnake.y[2] = newSnake.y[0];
                 break;
             case 3:
-                newSnake.moveDir = RIGHT;
-                newSnake.x[1] = newSnake.x[0] - 25;
+                newSnake.nextMoveDir = RIGHT;
+                newSnake.x[1] = newSnake.x[0] - OBJECT_SIZE;
                 newSnake.y[1] = newSnake.y[0];
-                newSnake.x[2] = newSnake.x[0] - 50;
+                newSnake.x[2] = newSnake.x[0] - (OBJECT_SIZE * 2);
                 newSnake.y[2] = newSnake.y[0];
                 break;
         }
 
+        newSnake.lastMoveDir = newSnake.nextMoveDir; 
+
         snakes.push(newSnake);
         for (let chunkNumber = 0; chunkNumber < newSnake.x.length; chunkNumber++) {
-            anotherChunk = createNewChunk(newSnake.x[chunkNumber], newSnake.y[chunkNumber], playerColors[i]);
+            anotherChunk = createNewChunk(newSnake.x[chunkNumber], newSnake.y[chunkNumber], playerClasses[i]);
             newSnake.chunks.push(anotherChunk);
             GAME.appendChild(anotherChunk); 
         }
     }
+}
+
+function start(players) {
+
+    // Clear the play space. 
+    clearScreen();
+
+    // Create the number of snakes we need and their 3 initial chunks. 
+    createSnakes(players); 
 
     addEventListener("keydown", function(e) {
         setMoveDir(e);
@@ -133,8 +237,8 @@ function start(players) {
 }
 
 function spawnFood() {
-    let x = clampDown(Math.random() * MAX_X_POS, 25);
-    let y = clampDown(Math.random() * MAX_Y_POS, 25);
+    let x = clampDown(Math.random() * MAX_X_POS, OBJECT_SIZE);
+    let y = clampDown(Math.random() * MAX_Y_POS, OBJECT_SIZE);
 
     let food = document.createElement("div"); 
     food.className = "food"; 
@@ -167,7 +271,7 @@ function setMoveDir(e) {
         }
     }
 
-    // Now deal with the key press. 
+    // Now deal with the key press - somehow lastMoveDir has broken this... 
     switch(e.which) {
 
         case ESCAPE:
@@ -175,86 +279,86 @@ function setMoveDir(e) {
             break; 
 
         case PLAYER_1_UP:
-            if (snakes[0].moveDir != DOWN) {
-                snakes[0].moveDir = UP;
+            if (snakes[0].lastMoveDir != DOWN) {
+                snakes[0].nextMoveDir = UP;
             }  
             break;
         case PLAYER_1_RIGHT:
-            if (snakes[0].moveDir != LEFT) {
-                snakes[0].moveDir = RIGHT;
+            if (snakes[0].lastMoveDir != LEFT) {
+                snakes[0].nextMoveDir = RIGHT;
             }
             break;
         case PLAYER_1_DOWN:
-            if (snakes[0].moveDir != UP) {
-                snakes[0].moveDir = DOWN;
+            if (snakes[0].lastMoveDir != UP) {
+                snakes[0].nextMoveDir = DOWN;
             }
             break;
         case PLAYER_1_LEFT:
-            if (snakes[0].moveDir != RIGHT) {
-                snakes[0].moveDir = LEFT;
+            if (snakes[0].lastMoveDir != RIGHT) {
+                snakes[0].nextMoveDir = LEFT;
             }
             break;
 
         case PLAYER_2_UP:
-            if (snakes[1].moveDir != DOWN) {
-                snakes[1].moveDir = UP;
+            if (snakes[1].lastMoveDir != DOWN) {
+                snakes[1].nextMoveDir = UP;
             }  
             break;
         case PLAYER_2_RIGHT:
-            if (snakes[1].moveDir != LEFT) {
-                snakes[1].moveDir = RIGHT;
+            if (snakes[1].lastMoveDir != LEFT) {
+                snakes[1].nextMoveDir = RIGHT;
             }
             break;
         case PLAYER_2_DOWN:
-            if (snakes[1].moveDir != UP) {
-                snakes[1].moveDir = DOWN;
+            if (snakes[1].lastMoveDir != UP) {
+                snakes[1].nextMoveDir = DOWN;
             }
             break;
         case PLAYER_2_LEFT:
-            if (snakes[1].moveDir != RIGHT) {
-                snakes[1].moveDir = LEFT;
+            if (snakes[1].lastMoveDir != RIGHT) {
+                snakes[1].nextMoveDir = LEFT;
             }
             break;
 
         case PLAYER_3_UP:
-            if (snakes[2].moveDir != DOWN) {
-                snakes[2].moveDir = UP;
+            if (snakes[2].lastMoveDir != DOWN) {
+                snakes[2].nextMoveDir = UP;
             }  
             break;
         case PLAYER_3_RIGHT:
-            if (snakes[2].moveDir != LEFT) {
-                snakes[2].moveDir = RIGHT;
+            if (snakes[2].lastMoveDir != LEFT) {
+                snakes[2].nextMoveDir = RIGHT;
             }
             break;
         case PLAYER_3_DOWN:
-            if (snakes[2].moveDir != UP) {
-                snakes[2].moveDir = DOWN;
+            if (snakes[2].lastMoveDir != UP) {
+                snakes[2].nextMoveDir = DOWN;
             }
             break;
         case PLAYER_3_LEFT:
-            if (snakes[2].moveDir != RIGHT) {
-                snakes[2].moveDir = LEFT;
+            if (snakes[2].lastMoveDir != RIGHT) {
+                snakes[2].nextMoveDir = LEFT;
             }
             break;
     
         case PLAYER_4_UP:
-            if (snakes[3].moveDir != DOWN) {
-                snakes[3].moveDir = UP;
+            if (snakes[3].lastMoveDir != DOWN) {
+                snakes[3].nextMoveDir = UP;
             }  
             break;
         case PLAYER_4_RIGHT:
-            if (snakes[3].moveDir != LEFT) {
-                snakes[3].moveDir = RIGHT;
+            if (snakes[3].lastMoveDir != LEFT) {
+                snakes[3].nextMoveDir = RIGHT;
             }
             break;
         case PLAYER_4_DOWN:
-            if (snakes[3].moveDir != UP) {
-                snakes[3].moveDir = DOWN;
+            if (snakes[3].lastMoveDir != UP) {
+                snakes[3].nextMoveDir = DOWN;
             }
             break;
         case PLAYER_4_LEFT:
-            if (snakes[3].moveDir != RIGHT) {
-                snakes[3].moveDir = LEFT;
+            if (snakes[3].lastMoveDir != RIGHT) {
+                snakes[3].nextMoveDir = LEFT;
             }
             break;
     }
@@ -272,18 +376,18 @@ function moveSnakes() {
         // Calculate predicted new position. 
         let newX = currentSnake.x[0];
         let newY = currentSnake.y[0]; 
-        switch(currentSnake.moveDir) {
+        switch(currentSnake.nextMoveDir) {
             case UP:
-                newY -= 25;
+                newY -= OBJECT_SIZE;
                 break;
             case RIGHT:
-                newX += 25;
+                newX += OBJECT_SIZE;
                 break;
             case DOWN:
-                newY += 25;
+                newY += OBJECT_SIZE;
                 break;
             case LEFT:
-                newX -= 25; 
+                newX -= OBJECT_SIZE; 
                 break;
         }
 
@@ -306,7 +410,7 @@ function moveSnakes() {
             let foodX = posToInt(food.style.left);
             let foodY = posToInt(food.style.top); 
             if (newX == foodX && newY == foodY) {
-                console.log("MY SNAKE IS GROWING!!"); 
+                currentSnake.score++;
                 supermarket.pop().remove();
                 currentSnake.targetLength++; 
                 spawnFood(); 
@@ -325,20 +429,22 @@ function moveSnakes() {
                     let otherX = posToInt(otherChunk.style.left);
                     let otherY = posToInt(otherChunk.style.top); 
                     if (newX == otherX && newY == otherY) {
-                        console.log("GAME OVER 4 U!!!!"); 
-                        currentSnake.active = false; 
-                        otherSnake.active = false;
-                        // clearInterval(moveInterval); 
+                        destroySnake(currentSnake);
+                        destroySnake(otherSnake);
+                        if (snakes.length < 1) {
+                            showEndScreen();
+                        }
+                        return;
                     }
                 }
             }
         }
 
-        // A new chunk is created and added onto the front of the array, with the newX and Y positions.
+        // If no collisions are detected, a new chunk is created and added onto the front of the array, with the newX and Y positions.
         currentSnake.x.unshift(newX);
         currentSnake.y.unshift(newY); 
 
-        let anotherChunk = createNewChunk(newX, newY, playerColors[i]);
+        let anotherChunk = createNewChunk(newX, newY, playerClasses[i]);
         currentSnake.chunks.unshift(anotherChunk);
         GAME.appendChild(anotherChunk); 
 
@@ -348,29 +454,25 @@ function moveSnakes() {
             currentSnake.y.pop(); 
             currentSnake.chunks.pop().remove();
         }
-        
 
-        // clearInterval(moveInterval);
+        currentSnake.lastMoveDir = currentSnake.nextMoveDir; 
     }
-
-    // Check for collisions - this is purely based on top/left style values. Everything will be on a grid, clamped to multiples of 25px. 
-
-    // If a player collides with themselves, or any other snake, they are dead m8! 
-
-    // If they collide with some YUMMY FOOD, their targetLength is incremented. 
-
-    // Depending on their movement direction, a new chunk is created and added onto the front of the array. 
-
-    // The last chunk on the end of the array, is removed - unless the amount of chunks is less than the targetLength!  
-
 }
 
-function createNewChunk(x, y, color) {
+function destroySnake(snake) {
+    snake.active = false;
+    for (let i = 0; i < snake.chunks.length; i++) {;
+        let chunk = snake.chunks[i];
+        chunk.className += " vibrate destroyed";
+        snake.chunks.pop().remove;
+    }
+}
+
+function createNewChunk(x, y, playerClass) {
     let newChunk = document.createElement("div");
-    newChunk.className = "snakeChunk"; 
+    newChunk.className = `${playerClass} snakeChunk`; 
     newChunk.style.left = intToPos(x);
     newChunk.style.top = intToPos(y);
-    newChunk.style.backgroundColor = color;
     return newChunk;
 }
 
